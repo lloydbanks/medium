2fd3c800de46af433e9b0ce89c4b10ee312ff097
Dmitry <dmitry.litvinov.spb@gmail.com>
2020-01-10 18:28:04 +0300

    add topBar

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes'
 import { BrowserRouter as Router } from 'react-router-dom'
+import TopBar from './components/topbar'
 
 const App = () => {
   return (
     <div>
-      <h1>Routes</h1>
       <Router>
+        <TopBar />
         <Routes />
       </Router>
     </div>
