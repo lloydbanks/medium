a8dc306d081cf9575edb80692f780b8bcd03b50e
Dmitry <dmitry.litvinov.spb@gmail.com>
2020-01-22 01:30:46 +0300

    add article form component

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import GlobalFeed from './pages/globalFeed'
 import TagFeed from './pages/tagFeed'
 import YourFeed from './pages/yourFeed'
 import Article from './pages/article'
+import CreateArticle from './pages/article/create'
 import Auth from './pages/auth'
 
 export default () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact />
+      <Route path="/articles/create" component={CreateArticle} />
       <Route path="/feed" component={YourFeed} />
       <Route path="/tags/:slug" component={TagFeed} />
       <Route path="/login" component={Auth} />
